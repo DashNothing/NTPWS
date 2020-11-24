a3c6793e6b670758e8b5ae2ff793af817afe1b27
DashNothing <stokic.toni@gmail.com>
2020-11-24 23:03:15 +0100

    Post form, post editing, post deletion

--- admin/posteditform.php
+++ admin/posteditform.php
@@ -9,27 +9,27 @@
 
   print '
 <form action="" name="registration_form" method="POST" enctype="multipart/form-data">
-  <input type="hidden" id="_action_" name="_action_" value="edit_news">
+  <input type="hidden" id="_action_" name="_action_" value="edit_post">
   <input type="hidden" id="edit" name="edit" value="' . $row['id'] . '">
 
   <label for="title">Title</label>
   <input type="text" id="title" name="title" value="' . $row['title'] . '" placeholder="Title..." required>
   
   <label for="description">Description</label>
-  <input type="text" id="description" name="description" value="' . $row['description'] . '" placeholder="Description..."
-    required>
+  <textarea rows=6 id="description" name="description" value="" placeholder="Description..."
+    required>' . $row['description'] . '</textarea>
 
   <label for="picture">Picture</label>
-  <input type="file" id="picture" name="picture" required>
-
-  <label for="username">Username *<small>(Username must have min 5 and max 10 char)</small></label>
-  <input type="text" id="username" name="username" value="' . $row['username'] . '" pattern=".{5,10}"
-    placeholder="Username.." required>
+  <input type="file" id="picture" name="picture[]" multiple accept="image/x-png,image/gif,image/jpeg" />
   
   <label for="archive">Archive</label>
-  <input type="radio" name="archive" value="Y"'; if ($row["archive"] == 'Y') {echo ' checked="checked"'; $checked_archive = true; } echo ' />Yes
-  <input type="radio" name="archive" value="N"'; if($checked_archive == false) { echo ' checked="checked"'; } echo ' /> NO
-  
+  <label class="radio-label">
+    <input type="radio" name="archive" value="Y"'; if ($row["archive"] == 'Y') {echo ' checked="checked"'; $checked_archive = true; } echo ' /> Yes
+  </label>
+  <label class="radio-label">
+    <input type="radio" name="archive" value="N"'; if($checked_archive == false) { echo ' checked="checked"'; } echo ' /> No
+  </label>
+
   <input type="submit" value="Submit">
 </form>
 ';
--- admin/posts.php
+++ admin/posts.php
@@ -1,5 +1,91 @@
 <?php
 
+  # Update post
+	if (isset($_POST['_action_']) && $_POST['_action_'] == 'edit_post') {
+		$query  = "UPDATE post SET title='" . htmlspecialchars($_POST['title'], ENT_QUOTES) . "', description='" . htmlspecialchars($_POST['description'], ENT_QUOTES) . "', archive='" . $_POST['archive'] . "'";
+    $query .= " WHERE id=" . (int)$_POST['edit'];
+    $query .= " LIMIT 1";
+    $result = @mysqli_query($MySQL, $query);
+    
+    # Pictures
+
+    # Get new uploaded pictures
+    $files = array_filter($_FILES['picture']['name']);
+
+    $total = count($_FILES['picture']['name']);
+
+    if (!empty($_FILES["picture"]["name"][0])) {
+      # Get all post pictures from the posts folder
+      $query = "SELECT * FROM picture";
+      $query .= " WHERE post=" . (int)$_GET['edit'];
+      $result = @mysqli_query($MySQL, $query);
+
+      while ($row = @mysqli_fetch_array($result)) {
+        @unlink("posts/" . $row["picture"]);
+      }
+
+      # Delete all previous post pictures from the db
+      $query  = "DELETE FROM picture";
+      $query .= " WHERE post=" . (int)$_GET['edit'];
+      $result = @mysqli_query($MySQL, $query);
+
+      for($i=0; $i < $total; $i++) {
+        //Get the temp file path
+        $tmpFilePath = $_FILES['picture']['tmp_name'][$i];
+      
+        //Make sure we have a file path
+        if ($tmpFilePath != "") {
+          $ext = strtolower(strrchr($_FILES['picture']['name'][$i], "."));
+              
+          $_picture = (int)$_POST['edit'] . '-' . rand(100000,999999) . $ext;
+      
+          //Upload the file into the posts dir
+          copy($tmpFilePath, "posts/" . $_picture);
+        }
+  
+        $_query  = "INSERT INTO picture SET picture='" . $_picture . "', post=" . $_POST["edit"];
+        $_result = @mysqli_query($MySQL, $_query);
+        $_SESSION['message'] .= '<p>You successfully added a picture.</p>';
+      }
+    }
+		
+		$_SESSION['message'] = '<p>Post successfully changed.</p>';
+		
+		# Redirect
+		header("Location: index.php?menu=9&action=2");
+	}
+  # End update news
+  
+
+  # Delete post
+  if (isset($_GET['delete']) && $_GET['delete'] != '') {
+    # Get all post pictures from the posts folder
+    $query = "SELECT * FROM picture";
+    $query .= " WHERE post=" . (int)$_GET['delete'];
+    $result = @mysqli_query($MySQL, $query);
+
+    while ($row = @mysqli_fetch_array($result)) {
+      @unlink("posts/" . $row["picture"]);
+    }
+
+		# Delete all post pictures from the db
+    $query  = "DELETE FROM picture";
+    $query .= " WHERE post=" . (int)$_GET['delete'];
+    $result = @mysqli_query($MySQL, $query);
+		
+		# Delete post
+		$query  = "DELETE FROM post";
+		$query .= " WHERE id=".(int)$_GET['delete'];
+		$query .= " LIMIT 1";
+		$result = @mysqli_query($MySQL, $query);
+
+		$_SESSION['message'] = '<p>Post successfully deleted.</p>';
+		
+		# Redirect
+		header("Location: index.php?menu=9&action=2");
+	}
+
+
   # Show posts table and forms only if user is admin or editor
   if (isset($_GET["edit"]) && $_GET["edit"] != "") {
     include("posteditform.php");
@@ -56,6 +142,8 @@
       <tbody>
       </table>
       </div>
+
+      <a href="index.php?menu=' . $menu . '&amp;action=' . $action . '&amp;add=true" class="primary-btn">Add a post</a>
     ';
   }
 ?>
